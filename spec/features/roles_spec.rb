6f9320da231e1ec36d244e3d0f79c255c97a513a
Andrew Vos <andrew.vos@gmail.com>
2016-09-22 11:30:54 +0100

    Add a role for "Resource Manager"
    This just points at the root for now.

---
+++ spec/features/roles_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature 'roles' do
+  context 'user without any permissions' do
+    let :user do
+      create(:user)
+    end
+
+    before do
+      GDS::SSO.test_user = user
+    end
+
+    scenario 'allows resource managers to manage resources' do
+      visit '/'
+      expect(page).to have_content(
+        'Sorry, you don\'t seem to have the resource_manager_permission permission for this app.'
+      )
+    end
+  end
+
+  context 'resource manager' do
+    let :user do
+      create(:resource_manager_user)
+    end
+
+    before do
+      GDS::SSO.test_user = user
+    end
+
+    scenario 'allows resource managers to manage resources' do
+      visit '/'
+      expect(page).to have_content 'Welcome'
+    end
+  end
+end
