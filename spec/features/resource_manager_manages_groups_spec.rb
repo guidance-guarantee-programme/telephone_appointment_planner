0cc0ab3d4daa13f720ca6f077e152b82e4720450
Ben Lovell <benjamin.lovell@gmail.com>
2016-09-29 17:41:44 +0100

    Add missing spec
    This was rebased out somehow. Derp.

--- spec/features/resource_manager_manages_groups_spec.rb
+++ spec/features/resource_manager_manages_groups_spec.rb
@@ -9,6 +9,15 @@ RSpec.feature 'Resource manager manages groups', js: true do
     end
   end
 
+  scenario 'Deleting a group' do
+    given_the_user_identifies_as_a_resource_manager do
+      and_there_are_existing_groups
+      when_they_visit_the_groups_page
+      and_they_delete_a_group
+      then_the_group_is_deleted
+    end
+  end
+
   def given_the_user_identifies_as_a_resource_manager
     @user = create(:resource_manager)
     GDS::SSO.test_user = @user
@@ -28,5 +37,13 @@ RSpec.feature 'Resource manager manages groups', js: true do
 
   def then_they_see_the_groups
     expect(@page).to have_groups(count: 1)
+  end
+
+  def and_they_delete_a_group
+    @page.groups.first.delete.click
+  end
+
+  def then_the_group_is_deleted
+    expect(@page).to have_no_groups
   end
 end
