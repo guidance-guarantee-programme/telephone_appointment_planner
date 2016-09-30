7199a0e54175969f0c50fa6bebea8e38e6dea780
Andrew Vos <andrew.vos@gmail.com>
2016-09-30 13:24:42 +0100

    Move feature spec steps to bottom

--- spec/features/roles_spec.rb
+++ spec/features/roles_spec.rb
@@ -1,51 +1,6 @@
 require 'rails_helper'
 
 RSpec.feature 'roles' do
-  def when_they_try_to_manager_guiders
-    @page = Pages::Users.new
-    @page.load
-  end
-
-  def when_they_try_to_manage_guiders_slots
-    @page = Pages::EditSchedule.new
-    @page.load(user_id: @guider.id, id: @schedule.id)
-  end
-
-  def and_a_guider_with_a_schedule_exists
-    @guider = create(:guider)
-    @schedule = @guider.schedules.create!(
-      from: 7.weeks.from_now
-    )
-    @schedule.slots.create(
-      day: 'Monday',
-      start_at: '09:00',
-      end_at: '10:30'
-    )
-  end
-
-  def and_a_guider_exists
-    @guider = create(:guider)
-  end
-
-  def then_they_can_manage_guiders_slots
-    @page = Pages::EditSchedule.new
-    @page.load(user_id: @guider.id, id: @schedule.id)
-    expect(@page).to be_displayed
-  end
-
-  def when_they_try_to_edit_guiders_schedules
-    @page = Pages::EditUser.new
-    @page.load(id: @guider.id)
-  end
-
-  def then_they_are_locked_out
-    expect(@page).to have_permission_error_message
-  end
-
-  def then_they_are_allowed
-    expect(@page).to_not have_permission_error_message
-  end
-
   scenario 'does not allow non-resource-managers to manage guiders' do
     given_the_user_has_no_permissions do
       when_they_try_to_manager_guiders
@@ -90,5 +45,50 @@ RSpec.feature 'roles' do
       when_they_try_to_manage_guiders_slots
       then_they_are_allowed
     end
+  end
+
+  def when_they_try_to_manager_guiders
+    @page = Pages::Users.new
+    @page.load
+  end
+
+  def when_they_try_to_manage_guiders_slots
+    @page = Pages::EditSchedule.new
+    @page.load(user_id: @guider.id, id: @schedule.id)
+  end
+
+  def and_a_guider_with_a_schedule_exists
+    @guider = create(:guider)
+    @schedule = @guider.schedules.create!(
+      from: 7.weeks.from_now
+    )
+    @schedule.slots.create(
+      day: 'Monday',
+      start_at: '09:00',
+      end_at: '10:30'
+    )
+  end
+
+  def and_a_guider_exists
+    @guider = create(:guider)
+  end
+
+  def then_they_can_manage_guiders_slots
+    @page = Pages::EditSchedule.new
+    @page.load(user_id: @guider.id, id: @schedule.id)
+    expect(@page).to be_displayed
+  end
+
+  def when_they_try_to_edit_guiders_schedules
+    @page = Pages::EditUser.new
+    @page.load(id: @guider.id)
+  end
+
+  def then_they_are_locked_out
+    expect(@page).to have_permission_error_message
+  end
+
+  def then_they_are_allowed
+    expect(@page).to_not have_permission_error_message
   end
 end
