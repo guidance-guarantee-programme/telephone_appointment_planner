cabb838d4ab33c8342a8e8cc295aa5115b288cdb
Ben Lovell <benjamin.lovell@gmail.com>
2016-09-30 11:37:43 +0100

    Search and display a guider's groups
    Displays group membership alongside the guider details. Also adds
    searchability to the groups fields for guiders.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   has_many :schedules, dependent: :destroy
 
+  has_and_belongs_to_many :groups # rubocop:disable Rails/HasAndBelongsToMany
+
   def self.guiders
     # This can't really be made faster because we're storing
     # permissions as a serialized string. We only really should have a maximum of
