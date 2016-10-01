f6bc8b1add978b0d95638f326f48c74bf5150cc1
Ben Lovell <benjamin.lovell@gmail.com>
2016-10-01 10:49:07 +0100

    Randomise test run ordering

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
 RSpec.configure do |config|
+  config.order = :random
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
