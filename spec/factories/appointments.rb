06a62410c5242b763527f729eee97c85208e0e03
Andrew Vos <andrew.vos@gmail.com>
2016-10-04 16:49:44 +0100

    Spec to ensure that clashing appointments aren't created

--- spec/factories/appointments.rb
+++ spec/factories/appointments.rb
@@ -1,4 +1,11 @@
 FactoryGirl.define do
   factory :appointment do
+    user { build(:guider) }
+    start_at { Time.zone.now }
+    end_at { Time.zone.now + 1.hour }
+    first_name { Faker::Name.name }
+    last_name { Faker::Name.name }
+    phone '932009320'
+    memorable_word 'lozenge'
   end
 end
--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
   describe 'validations' do
+    it 'is valid with valid parameters' do
+      appointment = create(:appointment)
+      expect(appointment).to be_valid
+    end
+
     required = [
       :user,
       :start_at,
@@ -20,7 +25,7 @@ RSpec.describe Appointment, type: :model do
   end
 
   describe '#assign_random_guider' do
-    context 'with a guider who has a slot available' do
+    context 'with a guider who has a slot at the appointment time' do
       let!(:guider_with_slot) do
         guider_with_slot = create(:guider)
         guider_with_slot.schedules.build(
@@ -45,6 +50,23 @@ RSpec.describe Appointment, type: :model do
         subject.end_at = Time.zone.now.change(hour: 10, min: 30)
         subject.assign_random_guider
         expect(subject.user).to eq guider_with_slot
+      end
+
+      context 'and the guider already has an appointment at that time' do
+        before do
+          guider_with_slot.appointments << build(
+            :appointment,
+            start_at: Time.zone.now.change(hour: 9, min: 0, sec: 0),
+            end_at: Time.zone.now.change(hour: 10, min: 30, sec: 0)
+          )
+        end
+
+        it 'does not assign a guider' do
+          subject.start_at = Time.zone.now.change(hour: 9, min: 0, sec: 0)
+          subject.end_at = Time.zone.now.change(hour: 10, min: 30, sec: 0)
+          subject.assign_random_guider
+          expect(subject.user).to eq nil
+        end
       end
     end
   end
