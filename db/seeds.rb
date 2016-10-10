7d16e70af845a6fa5ef2a84257f2e39e6239ee6a
Andrew Vos <andrew.vos@gmail.com>
2016-10-10 16:37:41 +0100

    Regenerate bookable slots when seeding
    Also ensure that everything in db:seed is only run in development.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,29 +4,31 @@ if Rails.env.development?
     user.permissions << User::AGENT_PERMISSION
     user.save!
   end
-end
 
-%w{early mid late}.each do |t|
-  User
-    .where(name: (1..15)
-    .map {|i| "#{t} shift #{i}"})
-    .destroy_all
-end
+  %w{early mid late}.each do |t|
+    User
+      .where(name: (1..15)
+      .map {|i| "#{t} shift #{i}"})
+      .destroy_all
+  end
 
-1.upto(15) do |n|
-  guider = FactoryGirl.create(:guider, name: "early shift #{n}")
-  schedule = FactoryGirl.build(:schedule, :with_early_shift, start_at: Time.zone.now, user: guider)
-  schedule.save!(validate: false)
-end
+  1.upto(15) do |n|
+    guider = FactoryGirl.create(:guider, name: "early shift #{n}")
+    schedule = FactoryGirl.build(:schedule, :with_early_shift, start_at: Time.zone.now, user: guider)
+    schedule.save!(validate: false)
+  end
 
-1.upto(15) do |n|
-  guider = FactoryGirl.create(:guider, name: "mid shift #{n}")
-  schedule = FactoryGirl.build(:schedule, :with_mid_shift, start_at: Time.zone.now, user: guider)
-  schedule.save!(validate: false)
-end
+  1.upto(15) do |n|
+    guider = FactoryGirl.create(:guider, name: "mid shift #{n}")
+    schedule = FactoryGirl.build(:schedule, :with_mid_shift, start_at: Time.zone.now, user: guider)
+    schedule.save!(validate: false)
+  end
 
-1.upto(15) do |n|
-  guider = FactoryGirl.create(:guider, name: "late shift #{n}")
-  schedule = FactoryGirl.build(:schedule, :with_late_shift, start_at: Time.zone.now, user: guider)
-  schedule.save!(validate: false)
+  1.upto(15) do |n|
+    guider = FactoryGirl.create(:guider, name: "late shift #{n}")
+    schedule = FactoryGirl.build(:schedule, :with_late_shift, start_at: Time.zone.now, user: guider)
+    schedule.save!(validate: false)
+  end
+
+  BookableSlot.regenerate_for_six_weeks
 end
