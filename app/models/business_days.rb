d99bf9cf6c96076f88223584f6a7342bd4c6e9f6
Andrew Vos <andrew.vos@gmail.com>
2016-10-17 10:50:45 +0100

    Fix biz crashing on POST appointment_attempts/:id/appointments

--- app/models/business_days.rb
+++ app/models/business_days.rb
@@ -1,22 +1,26 @@
 class BusinessDays
-  # This can't happen in an initializer because:
-  # https://github.com/zendesk/biz/issues/18
-  Biz.configure do |config|
-    config.hours = {
-      mon: { '08:00' => '17:30' },
-      tue: { '08:00' => '17:30' },
-      wed: { '08:00' => '17:30' },
-      thu: { '08:00' => '17:30' },
-      fri: { '08:00' => '17:30' }
-    }
-    config.holidays = []
-  end
-
   def self.from_now(amount)
+    configure
     Biz.time(amount, :day).after(Time.zone.now)
   end
 
   def self.before_now(amount)
+    configure
     Biz.time(amount, :day).before(Time.zone.now)
+  end
+
+  # This can't happen in an initializer because:
+  # https://github.com/zendesk/biz/issues/18
+  def self.configure
+    @configuration ||= Biz.configure do |config|
+      config.hours = {
+        mon: { '08:00' => '17:30' },
+        tue: { '08:00' => '17:30' },
+        wed: { '08:00' => '17:30' },
+        thu: { '08:00' => '17:30' },
+        fri: { '08:00' => '17:30' }
+      }
+      config.holidays = []
+    end
   end
 end
