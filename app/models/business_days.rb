a58b8181a770fc5e708444dbe5f66f6ec624e976
Andrew Vos <andrew.vos@gmail.com>
2016-10-17 11:35:00 +0100

    Change Biz to working_hours

--- app/models/business_days.rb
+++ app/models/business_days.rb
@@ -1,26 +1,9 @@
 class BusinessDays
   def self.from_now(amount)
-    configure
-    Biz.time(amount, :day).after(Time.zone.now)
+    amount.working.day.from_now
   end
 
   def self.before_now(amount)
-    configure
-    Biz.time(amount, :day).before(Time.zone.now)
-  end
-
-  # This can't happen in an initializer because:
-  # https://github.com/zendesk/biz/issues/18
-  def self.configure
-    @configuration ||= Biz.configure do |config|
-      config.hours = {
-        mon: { '08:00' => '17:30' },
-        tue: { '08:00' => '17:30' },
-        wed: { '08:00' => '17:30' },
-        thu: { '08:00' => '17:30' },
-        fri: { '08:00' => '17:30' }
-      }
-      config.holidays = []
-    end
+    amount.working.day.ago
   end
 end
