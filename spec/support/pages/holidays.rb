00886f875d5a9355a5743a5ac9a47a1338a2fc43
Andrew Vos <andrew.vos@gmail.com>
2016-10-19 16:34:08 +0100

    Add tests for viewing holidays

--- spec/support/pages/holidays.rb
+++ spec/support/pages/holidays.rb
@@ -6,5 +6,22 @@ module Pages
       'h1',
       text: 'Sorry, you don\'t seem to have the resource_manager permission for this app.'
     )
+
+    element :next_week, '.fc-next-button'
+
+    sections :appointments, '.fc-event' do
+      element :title, '.fc-title'
+      element :time, '.fc-time'
+    end
+
+    def all_appointments
+      wait_for_appointments
+      appointments.map do |a|
+        {
+          title: a.title.text,
+          time: a.time.text
+        }
+      end
+    end
   end
 end
