5599b1c88fb3f45916e7789e99301f0fff139ff8
Ben Lovell <benjamin.lovell@gmail.com>
2016-11-08 10:45:45 +0000

    Improve date / time handling
    Corrects some unnecessary coercions when handling dates / times.

--- spec/jobs/pusher_notification_job_spec.rb
+++ spec/jobs/pusher_notification_job_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe PusherNotificationJob, '#perform' do
   subject { described_class.new.perform(0, appointment) }
 
   context 'when the appointment is for today' do
-    let(:appointment) { build_stubbed(:appointment, start_at: Time.zone.today) }
+    let(:appointment) { build_stubbed(:appointment, start_at: Time.zone.now) }
 
     it 'sends the push notification' do
       expect(Pusher).to receive(:trigger)
@@ -14,7 +14,7 @@ RSpec.describe PusherNotificationJob, '#perform' do
   end
 
   context 'when the appointment is for another day' do
-    let(:appointment) { build_stubbed(:appointment, start_at: Time.zone.tomorrow) }
+    let(:appointment) { build_stubbed(:appointment, start_at: 1.day.from_now) }
 
     it 'does nothing' do
       expect(Pusher).to_not receive(:trigger)
