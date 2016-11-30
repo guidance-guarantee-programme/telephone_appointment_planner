bc23d839a61ccb43cb54a8684a6ff405c409d9f5
Andrew Vos <andrew.vos@gmail.com>
2016-11-30 13:42:47 +0000

    When deleting groups, destroy groups that aren't in use by anyone

--- app/forms/destroy_group_assignments.rb
+++ app/forms/destroy_group_assignments.rb
@@ -8,10 +8,17 @@ class DestroyGroupAssignments
     GroupAssignment
       .where(user_id: user_ids, group_id: group_id)
       .delete_all
+
+    destroy_orphaned_group
   end
 
   private
 
   attr_reader :user_ids
   attr_reader :group_id
+
+  def destroy_orphaned_group
+    group = Group.find(group_id)
+    group.destroy if group.group_assignments.none?
+  end
 end
