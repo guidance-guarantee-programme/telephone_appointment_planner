3ddeea297cfb0140c6157b5410e448f1a7982bf3
Ben Lovell <benjamin.lovell@gmail.com>
2016-12-08 14:46:07 +0000

    Reduce huge N+1 querying for associated user IDs
    This was causing a huge N+1 query through groups / assignments / users.

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,9 +1,10 @@
 module UserHelper
   def guider_options
     groups = Group.includes(:users)
+
     {
       'Groups': groups.map do |group|
-        [group.name, group.name, data: { icon: 'glyphicon-tag', children_to_select: group.users.pluck(:id) }]
+        [group.name, group.name, data: { icon: 'glyphicon-tag', children_to_select: group.user_ids }]
       end,
       'Users': User.guiders.active.map do |guider|
         [guider.name, guider.id, data: { icon: 'glyphicon-user' }]
