9fcdddd65f7a2fb3e5ad684bb15f9f5e58224e0f
Ben Lovell <benjamin.lovell@gmail.com>
2016-12-08 14:54:11 +0000

    Tidy up options styling
    This code wasn't particularly idiomatic ruby, regardless whether
    rubocop picked it up.

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -3,12 +3,33 @@ module UserHelper
     groups = Group.includes(:users)
 
     {
-      'Groups': groups.map do |group|
-        [group.name, group.name, data: { icon: 'glyphicon-tag', children_to_select: group.user_ids }]
-      end,
-      'Users': User.guiders.active.map do |guider|
-        [guider.name, guider.id, data: { icon: 'glyphicon-user' }]
-      end
+      'Groups' => group_options(groups),
+      'Users'  => user_options
     }
+  end
+
+  def group_options(groups)
+    groups.map do |group|
+      [
+        group.name,
+        group.name,
+        data: {
+          icon: 'glyphicon-tag',
+          children_to_select: group.user_ids
+        }
+      ]
+    end
+  end
+
+  def user_options
+    User.guiders.active.map do |guider|
+      [
+        guider.name,
+        guider.id,
+        data: {
+          icon: 'glyphicon-user'
+        }
+      ]
+    end
   end
 end
