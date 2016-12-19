396e0c99ecde5090aaad12b53e12b0edae09fa97
Andrew Vos <andrew.vos@gmail.com>
2016-12-19 13:56:38 +0000

    Fix HTML injection vulnerability on Appointments#search
    Remove old code that was used when we handled search result highlighting
    in the backend

--- app/presenters/appointment_search_result_presenter.rb
+++ app/presenters/appointment_search_result_presenter.rb
@@ -1,6 +1,8 @@
 class AppointmentSearchResultPresenter < SimpleDelegator
   DATE_FORMAT = '%d %B %Y %H:%M'.freeze
 
+  delegate :name, to: :guider, prefix: true
+
   def status
     super.titleize
   end
@@ -11,21 +13,5 @@ class AppointmentSearchResultPresenter < SimpleDelegator
 
   def date
     start_at.strftime(DATE_FORMAT)
-  end
-
-  def id
-    self[:highlighted_id] || super.to_s
-  end
-
-  def first_name
-    self[:highlighted_first_name] || super
-  end
-
-  def last_name
-    self[:highlighted_last_name] || super
-  end
-
-  def guider_name
-    self[:highlighted_name] || guider.name
   end
 end
