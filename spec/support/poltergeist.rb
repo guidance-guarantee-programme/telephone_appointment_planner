1ea0a2e0f34c8abe0d955bc2290d898f0daf20b7
Ben Lovell <benjamin.lovell@gmail.com>
2016-12-28 12:36:17 +0000

    Correct pusher asset blacklisting during specs
    This yields a good improvement as the specs aren't unnecessarily
    waiting around for pusher's analytics assets. It does however highlight
    even more timing issues in our specs that were being masked by the long
    wait times for these assets.

--- spec/support/poltergeist.rb
+++ spec/support/poltergeist.rb
@@ -1,4 +1,8 @@
 require 'capybara/poltergeist'
 
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, url_blacklist: %w(/timeline/v2))
+end
+
 Capybara.javascript_driver = :poltergeist
 Capybara.default_max_wait_time = 10 if ENV['TRAVIS']
