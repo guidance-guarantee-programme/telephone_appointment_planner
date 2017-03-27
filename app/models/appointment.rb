d1271628d6b4f127d2ce82a1a1aac106457432b9
Ben Lovell <benjamin.lovell@gmail.com>
2017-03-27 17:34:06 +0100

    Calculate grace period from close of play
    We were previously calculating the two business days grace period from
    the current date and time. We should have calculated this from the close
    of play and appointments have inadvertently been created.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -144,7 +144,7 @@ class Appointment < ApplicationRecord
   def not_within_two_business_days
     return unless new_record? && start_at?
 
-    too_soon = start_at < BusinessDays.from_now(2)
+    too_soon = start_at < BookableSlot.next_valid_start_date
     errors.add(:start_at, 'must be more than two business days from now') if too_soon
   end
 
