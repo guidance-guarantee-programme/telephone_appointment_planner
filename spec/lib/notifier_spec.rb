75862d617a55af61b178998f5decfae481623fa5
Ben Lovell <benjamin.lovell@gmail.com>
2017-03-30 14:16:40 +0100

    Ensure activities are only created when necessary
    While the actual email was a noop thanks to the absence of a customer
    email address on the associated appointment we were incorrectly creating
    activities. This change ensures activities are only created (and skips a
    heap of other machinery) when a customer email address is present.

--- spec/lib/notifier_spec.rb
+++ spec/lib/notifier_spec.rb
@@ -16,6 +16,19 @@ RSpec.describe Notifier, '#call' do
     allow(PusherActivityCreatedJob).to receive(:perform_later)
   end
 
+  context 'when the appointment is without an associated email' do
+    before do
+      stub_const('AppointmentMailer', double)
+      stub_const('CustomerUpdateActivity', double)
+    end
+
+    it 'does not notify the customer' do
+      appointment.update_attribute(:email, '')
+
+      subject.call
+    end
+  end
+
   context 'and I update a core detail' do
     before { appointment.update_attribute(:first_name, 'Jean Ralphio') }
 
