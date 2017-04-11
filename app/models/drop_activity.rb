aa2a868cb649c25ddb1b8bcbd7deefe9f1b5d27e
Ben Lovell <benjamin.lovell@gmail.com>
2017-04-11 11:11:52 +0100

    Ensure notifications are directed accurately
    In the case of dropped summary documents these are always the guider's
    responsibility to action, so only they will receive the high-priority
    notifications.
    
    In the case of other dropped emails - for now we will notify both the
    guider and the agent - so long as the agent is not the Pension Wise
    website API agent. This is despite the `owner` of the activity always
    being the guider.
    
    Since these two activities are always considered 'high priority' we can
    do away with the guard and move the event hooks into the concrete
    activity classes.

--- app/models/drop_activity.rb
+++ app/models/drop_activity.rb
@@ -1,4 +1,12 @@
 class DropActivity < Activity
+  after_commit do
+    PusherHighPriorityCountChangedJob.perform_later(owner)
+
+    unless appointment.agent_is_pension_wise_api?
+      PusherHighPriorityCountChangedJob.perform_later(appointment.agent)
+    end
+  end
+
   def self.from(event, description, appointment)
     create!(
       message: "#{event.humanize} - #{description}",
