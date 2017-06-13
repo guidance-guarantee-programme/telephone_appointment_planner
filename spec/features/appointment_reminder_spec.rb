b91466c5bc8ef8d2e0f0fa860ce0574905c0eef8
Jon Gilbraith <jon@hobocode.io>
2017-06-13 12:36:42 +0100

    Refactor some brittle tests using nested `travel_to`
    Can no longer get away with that antipattern

--- spec/features/appointment_reminder_spec.rb
+++ spec/features/appointment_reminder_spec.rb
@@ -2,23 +2,39 @@ require 'rails_helper'
 
 RSpec.feature 'Appointment reminders' do
   scenario 'when less than 48 hours before the appointment it sends a reminder' do
-    travel_to Time.zone.parse('2016-06-13 12:00') do
-      given_an_appointment_exists
+    given_it_is_a_monday_not_near_a_bank_holiday do
+      and_an_appointment_exists
 
-      travel_to Time.zone.parse('2016-06-18 11:55') do
-        when_the_reminder_job_runs
+      when_it_is_less_than_48_hours_before_the_appointment do
+        and_the_reminder_job_runs
         then_no_email_reminder_is_delivered
         and_no_reminder_activity_is_logged
       end
     end
   end
 
-  def given_an_appointment_exists
+  def given_it_is_a_monday_not_near_a_bank_holiday
+    travel_to Time.zone.parse('2016-06-13 12:00')
+
+    yield
+
+    travel_back
+  end
+
+  def and_an_appointment_exists
     @start_at = Time.zone.parse('2016-06-20 12:00')
     @appointment = create(:appointment, start_at: @start_at)
   end
 
-  def when_the_reminder_job_runs
+  def when_it_is_less_than_48_hours_before_the_appointment
+    travel_to Time.zone.parse('2016-06-18 11:55')
+
+    yield
+
+    travel_back
+  end
+
+  def and_the_reminder_job_runs
     AppointmentRemindersJob.new.perform
   end
 
