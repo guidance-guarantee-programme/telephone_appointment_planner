38421f95a39449d16e819c0b9095375d98ce7dca
Ben Lovell <benjamin.lovell@gmail.com>
2018-04-03 19:49:45 +0100

    Direct SFTP traffic through proxy
    This allows us to punch a hole through the receiving service's firewall.

--- app/lib/batch_print_ftp.rb
+++ app/lib/batch_print_ftp.rb
@@ -1,18 +1,23 @@
+require 'uri'
+require 'net/ssh'
 require 'net/sftp'
+require 'net/ssh/proxy/http'
 
 class BatchPrintFtp
   def initialize(
-    host:     ENV['FTP_HOST'],
-    username: ENV['FTP_USERNAME'],
-    password: ENV['FTP_PASSWORD']
+    host:      ENV['FTP_HOST'],
+    username:  ENV['FTP_USERNAME'],
+    password:  ENV['FTP_PASSWORD'],
+    proxy_uri: ENV['QUOTAGUARDSTATIC_URL']
   )
-    @host     = host
-    @username = username
-    @password = password
+    @host      = host
+    @username  = username
+    @password  = password
+    @proxy_uri = URI(proxy_uri)
   end
 
   def call(data)
-    Net::SFTP.start(host, username, password: password) do |sftp|
+    Net::SFTP.start(host, username, password: password, proxy: proxy) do |sftp|
       sftp.file.open(filename, 'w') do |file|
         file.puts(data)
       end
@@ -21,6 +26,15 @@ class BatchPrintFtp
 
   private
 
+  def proxy
+    Net::SSH::Proxy::HTTP.new(
+      proxy_uri.host,
+      proxy_uri.port,
+      user: proxy_uri.user,
+      password: proxy_uri.password
+    )
+  end
+
   def filename
     "#{Time.zone.today}.csv"
   end
@@ -28,4 +42,5 @@ class BatchPrintFtp
   attr_reader :host
   attr_reader :username
   attr_reader :password
+  attr_reader :proxy_uri
 end
