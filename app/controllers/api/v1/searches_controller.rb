66aa9b0081e3612eafddf3886b1416107e97b27e
Ben Lovell <benjamin.lovell@gmail.com>
2020-02-27 14:46:48 +0000

    Authorise API users only

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class SearchesController < ApplicationController
+      before_action :authorise_for_api_users!
+
       def index
         @results = AppointmentApiSearch.new(params[:query]).call
 
