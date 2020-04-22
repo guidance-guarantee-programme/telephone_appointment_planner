a713d1bd768817cbf6383a42905919eaa08ca5c6
Ben Lovell <benjamin.lovell@gmail.com>
2020-04-22 15:33:21 +0100

    Extend close-of-play to 9pm
    Effectively set the CoP to 9pm. This was 6:30pm previously and would
    cause confusion among some delivery providers.

--- app/models/bookable_slot.rb
+++ app/models/bookable_slot.rb
@@ -17,7 +17,7 @@ class BookableSlot < ApplicationRecord
   def self.next_valid_start_date(user = nil)
     return Time.zone.now.advance(hours: 1) if user && user.resource_manager?
 
-    BusinessDays.from_now(1).change(hour: 18, min: 30).in_time_zone('London')
+    BusinessDays.from_now(1).change(hour: 21, min: 0).in_time_zone('London')
   end
 
   def self.find_available_slot(start_at, agent)
