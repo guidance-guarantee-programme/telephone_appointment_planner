10ed7e5d1bd71beb27a03e178cadad96bfb7a004
Ben Lovell <benjamin.lovell@gmail.com>
2020-04-26 15:17:25 +0100

    Bump TPAS-specific grace period to 48 hours
    Per request from TPAS.

--- app/models/bookable_slot.rb
+++ app/models/bookable_slot.rb
@@ -17,7 +17,7 @@ class BookableSlot < ApplicationRecord
   end
 
   def self.limit_by_organisation(from, to) # rubocop:disable MethodLength
-    tpas_start_at = BusinessDays.from_now(4).change(hour: 18, min: 30).in_time_zone('London')
+    tpas_start_at = BusinessDays.from_now(2).change(hour: 21, min: 0).in_time_zone('London')
 
     joins(:guider)
       .where(
