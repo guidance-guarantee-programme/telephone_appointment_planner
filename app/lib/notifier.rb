6120aae9e0510c4aac9195fac6b1a11011035a30
Ben Lovell <benjamin.lovell@gmail.com>
2020-08-17 14:48:50 +0100

    Rescheduling casebook appointments
    When a casebook appointment is rescheduled, the original appointment is
    cancelled in casebook and a new one is created in its place.

--- app/lib/notifier.rb
+++ app/lib/notifier.rb
@@ -27,7 +27,11 @@ class Notifier # rubocop:disable Metrics/ClassLength
   private
 
   def notify_casebook
-    CancelCasebookAppointmentJob.perform_later(appointment) if appointment_cancelled?
+    if appointment_cancelled?
+      CancelCasebookAppointmentJob.perform_later(appointment)
+    elsif appointment_rescheduled?
+      RescheduleCasebookAppointmentJob.perform_later(appointment)
+    end
   end
 
   def notify_resource_managers
