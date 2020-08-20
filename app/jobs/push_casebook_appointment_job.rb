b3c8605d78f5c9cf2eabf935c29bd6c1074acd18
Ben Lovell <benjamin.lovell@gmail.com>
2020-08-20 14:53:35 +0100

    Handle rebooks
    When an appointment is rebooked and the original appointment was pushed
    to casebook it should be cancelled after the new appointment is pushed.

--- app/jobs/push_casebook_appointment_job.rb
+++ app/jobs/push_casebook_appointment_job.rb
@@ -3,5 +3,15 @@ class PushCasebookAppointmentJob < CasebookJob
     return unless appointment.push_to_casebook?
 
     Casebook::Push.new(appointment).call
+
+    return unless casebook_rebooked?(appointment)
+
+    Casebook::Cancel.new(appointment.rebooked_from).call
+  end
+
+  private
+
+  def casebook_rebooked?(appointment)
+    appointment.rebooked_from&.casebook_appointment_id?
   end
 end
--- spec/jobs/push_casebook_appointment_job_spec.rb
+++ spec/jobs/push_casebook_appointment_job_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe PushCasebookAppointmentJob, '#perform' do
   context 'when the appointment is pushable' do
-    let(:appointment) { double(:appointment, push_to_casebook?: true) }
+    let(:appointment) { double(:appointment, rebooked_from: nil, push_to_casebook?: true) }
     let(:casebook_push) { instance_double(Casebook::Push) }
+    let(:casebook_cancel) { instance_double(Casebook::Cancel) }
 
     before do
       allow(Casebook::Push).to receive(:new).with(appointment).and_return(casebook_push)
@@ -15,6 +16,24 @@ RSpec.describe PushCasebookAppointmentJob, '#perform' do
       described_class.perform_now(appointment)
 
       expect(casebook_push).to have_received(:call)
+    end
+
+    context 'when the appointment was rebooked from a casebook appointment' do
+      let(:appointment) { create(:appointment, :casebook_rebooked, :casebook_guider) }
+
+      before do
+        allow(Casebook::Cancel).to receive(:new).with(appointment.rebooked_from).and_return(casebook_cancel)
+      end
+
+      it 'cancels the original appointment in casebook' do
+        allow(casebook_push).to receive(:call)
+        allow(casebook_cancel).to receive(:call)
+
+        described_class.perform_now(appointment)
+
+        expect(casebook_cancel).to have_received(:call)
+        expect(casebook_push).to have_received(:call)
+      end
     end
 
     context 'when a casebook error occurs' do
