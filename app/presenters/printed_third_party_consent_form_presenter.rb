23470d7480e434ae0295122c9151aef338d5b640
Ben Lovell <benjamin.lovell@gmail.com>
2020-10-23 12:07:17 +0100

    Customise printed letter for CAS appointments
    CAS appointments have slightly different terms for data retention so we
    need to pass customisation options to notify.

--- app/presenters/printed_third_party_consent_form_presenter.rb
+++ app/presenters/printed_third_party_consent_form_presenter.rb
@@ -5,6 +5,7 @@ class PrintedThirdPartyConsentFormPresenter
 
   def to_h # rubocop:disable MethodLength, AbcSize
     {
+      cas: cas_guider,
       reference: appointment.to_param,
       third_party_name: appointment.name,
       address_line_1: appointment.data_subject_name,
@@ -20,4 +21,8 @@ class PrintedThirdPartyConsentFormPresenter
   private
 
   attr_reader :appointment
+
+  def cas_guider
+    appointment.cas_guider? ? 'Yes' : 'No'
+  end
 end
