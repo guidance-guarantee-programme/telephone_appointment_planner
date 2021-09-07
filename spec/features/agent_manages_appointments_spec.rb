15db723007ccfad331594559ee67150fb1c9b301
Ben Lovell <benjamin.lovell@gmail.com>
2021-09-07 08:39:58 +0100

    Ensure TPAS see PW slots when booking for PW
    Similarly to the prior commit, ensure TPAS users see PW slots unless
    attempting another type of booking.

--- spec/features/agent_manages_appointments_spec.rb
+++ spec/features/agent_manages_appointments_spec.rb
@@ -16,6 +16,17 @@ RSpec.feature 'Agent manages appointments' do
     end
   end
 
+  scenario 'TPAS user booking a Pension Wise appointment', js: true do
+    travel_to '2021-04-05 10:00' do
+      given_the_user_is_a_resource_manager(organisation: :tpas) do
+        and_slots_exist_for_general_availability
+        and_slots_exist_for_due_diligence_availability
+        when_they_attempt_to_book_an_appointment
+        then_they_see_only_general_availability
+      end
+    end
+  end
+
   def and_slots_exist_for_due_diligence_availability
     create(:bookable_slot, :due_diligence, start_at: Time.zone.parse('2021-04-08 14:30'))
   end
