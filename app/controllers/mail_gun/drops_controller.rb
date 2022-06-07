f23d568e952b85828f86e2e6bb09d85269405172
Andrew Vos <andrew.vos@gmail.com>
2022-06-07 16:57:44 +0100

    Log btinternet mail drops
    So that logentries can send us notifications

--- app/controllers/mail_gun/drops_controller.rb
+++ app/controllers/mail_gun/drops_controller.rb
@@ -6,10 +6,18 @@ module MailGun
       form = DropForm.new(drop_params)
       form.create_activity
 
+      log_btinternet_drops
+
       head :ok
     end
 
     private
+
+    def log_btinternet_drops
+      return unless drop_params[:description].include? 'btinternet'
+
+      Rails.logger.info 'Mail drop for @btinternet'
+    end
 
     def drop_params
       params.permit(
