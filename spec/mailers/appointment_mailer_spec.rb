a25b78fbc6ed6801fd9bab9f4915a17ff2d7550d
Ben Lovell <benjamin.lovell@gmail.com>
2023-07-07 12:56:09 +0100

    Update BSL exit poll notifications

--- spec/mailers/appointment_mailer_spec.rb
+++ spec/mailers/appointment_mailer_spec.rb
@@ -57,7 +57,7 @@ RSpec.describe AppointmentMailer, type: :mailer do
     end
 
     it 'renders the exit poll vanity URL' do
-      expect(subject.body.encoded).to match('https://www.pensionwise.gov.uk/bsl-exit-poll')
+      expect(subject.body.encoded).to match('https://actiondeafness.org.uk/feedback-form')
     end
   end
 
