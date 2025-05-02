bbb2a8636e63ce90f3f31dad6975f8dcbc768c01
Ben Lovell <benjamin.lovell@gmail.com>
2025-05-02 09:00:17 +0100

    Rubocop

--- config/initializers/new_framework_defaults_7_0.rb
+++ config/initializers/new_framework_defaults_7_0.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Layout/LineLength
 # Be sure to restart your server when you modify this file.
 #
 # This file eases your Rails 7.0 framework defaults upgrade.
@@ -92,7 +93,6 @@
 #   "Referrer-Policy" => "strict-origin-when-cross-origin"
 # }
 
-
 # ** Please read carefully, this must be configured in config/application.rb **
 # Change the format of the cache entry.
 # Changing this default means that all new cache entries added to the cache
@@ -101,7 +101,6 @@
 # and you have no plans to rollback.
 # When you're ready to change format, add this to `config/application.rb` (NOT this file):
 #  config.active_support.cache_format_version = 7.0
-
 
 # Cookie serializer: 2 options
 #
@@ -141,3 +140,4 @@
 # Disables the deprecated #to_s override in some Ruby core classes
 # See https://guides.rubyonrails.org/configuring.html#config-active-support-disable-to-s-conversion for more information.
 # config.active_support.disable_to_s_conversion = true
+# rubocop:enable Layout/LineLength
