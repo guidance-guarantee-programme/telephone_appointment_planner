c77c16f14e4783766671db3c5cba56f76755feb3
Ben Lovell <benjamin.lovell@gmail.com>
2025-07-04 16:49:00 +0100

    Handle Casebook reschedules correctly
    This covers all combinations of appointment attributes and does the
    correct action depending on what's required for Casebook integration.

--- app/jobs/reschedule_casebook_appointment_job.rb
+++ app/jobs/reschedule_casebook_appointment_job.rb
@@ -1,7 +1,27 @@
 class RescheduleCasebookAppointmentJob < CasebookJob
   def perform(appointment)
-    return unless appointment.casebook_pushable_guider? && appointment.casebook_appointment_id?
+    return if ignore?(appointment)
 
-    Casebook::Reschedule.new(appointment).call
+    if unpushed_new_casebook_appointment?(appointment)
+      Casebook::Push.new(appointment).call
+    elsif pushed_casebook_appointment_reallocated_to_non_casebook_guider?(appointment)
+      Casebook::Cancel.new(appointment).call
+    else
+      Casebook::Reschedule.new(appointment).call
+    end
+  end
+
+  private
+
+  def unpushed_new_casebook_appointment?(appointment)
+    !appointment.casebook_appointment_id? && appointment.casebook_pushable_guider?
+  end
+
+  def pushed_casebook_appointment_reallocated_to_non_casebook_guider?(appointment)
+    appointment.casebook_appointment_id? && !appointment.casebook_pushable_guider?
+  end
+
+  def ignore?(appointment)
+    !appointment.casebook_pushable_guider? && !appointment.casebook_appointment_id?
   end
 end
