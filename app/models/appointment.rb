d94b543c0a335bcdeb5f5ac690d342d488523749
Ben Lovell <benjamin.lovell@gmail.com>
2026-02-06 17:25:13 +0000

    Extend booking window to 60 days

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -661,7 +661,7 @@ class Appointment < ApplicationRecord
   def valid_within_booking_window
     return unless start_at && start_at_changed? && ad_hoc_start_at.blank?
 
-    too_late = start_at > BookableSlot.end_of_window_for(schedule_type)
+    too_late = start_at > BookableSlot.end_of_window
     errors.add(:start_at, 'must not exceed the booking window') if too_late
   end
 
