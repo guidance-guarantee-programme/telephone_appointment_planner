0c96b71a4ee1bb9157e61d754749b0fcc4d2c415
Ben Lovell <benjamin.lovell@gmail.com>
2026-03-11 16:49:44 +0000

    Don't notify adjustments for notes

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -304,15 +304,14 @@ class Appointment < ApplicationRecord
     self.rescheduled_at     = Time.zone.now
   end
 
-  # rubocop:disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
+  # rubocop:disable Metrics/CyclomaticComplexity
   def adjustments?
     return true if accessibility_requirements? || third_party_booking? || extended_duration?
     return true if bsl_video? || welsh? || ms_teams_call?
-    return true if notes?
 
     !dc_pot_confirmed? && pension_wise?
   end
-  # rubocop:enable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
+  # rubocop:enable Metrics/CyclomaticComplexity
 
   def address?
     [address_line_one, town, postcode].all?(&:present?)
