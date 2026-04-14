70d73a7e27be180dbebd228c72a84ba9f376733e
Ben Lovell <benjamin.lovell@gmail.com>
2026-04-14 10:49:10 +0100

    Improve Puma config and remove warnings

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,8 @@
 environment ENV.fetch('RACK_ENV', 'development')
-workers     ENV.fetch('WEB_CONCURRENCY', 1)
+workers     @workers = ENV.fetch('WEB_CONCURRENCY', 1)
 threads     ENV.fetch('RAILS_MAX_THREADS', 5), ENV.fetch('RAILS_MAX_THREADS', 5)
 port        ENV.fetch('PORT', 3000)
 
 preload_app!
 
-on_worker_boot do
-  ActiveRecord::Base.establish_connection
-end
+before_worker_boot { ActiveRecord::Base.establish_connection } if @workers.to_i > 1
