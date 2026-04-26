f31aacf60ccba3eb8be5fb79bb8dec60025cd88d
Ben Lovell <benjamin.lovell@gmail.com>
2026-04-26 19:05:11 +0100

    Add further columns to online reschedulings

--- spec/support/pages/online_reschedulings_search.rb
+++ spec/support/pages/online_reschedulings_search.rb
@@ -7,6 +7,7 @@ module Pages
       element :customer_name, '.t-name'
       element :previous_guider_name, '.t-previous-guider-name'
       element :appointment_date_time, '.t-appointment-date-time'
+      element :appointment_new_date_time, '.t-appointment-new-date-time'
       element :rescheduled_date_time, '.t-rescheduled-date-time'
       element :processed, '.t-processed'
     end
